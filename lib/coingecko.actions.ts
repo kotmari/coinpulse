4bc939a373561f83ba8f0345d14236ad464191c9
kotmari <mariannakot79@gmail.com>
2026-01-07 17:59:39 +0200

    implemetnt coin-details page

--- lib/coingecko.actions.ts
+++ lib/coingecko.actions.ts
@@ -32,3 +32,25 @@ if(!API_KEY) throw new Error ("Could not get api key")
 
       return response.json()
    }
+
+   export async function getPools(id: string, network?: string | null, contractAddress?: string | null): Promise<PoolData>{
+      const fallback: PoolData = {
+         id: '',
+         network: '',
+         name: '',
+         address: ''
+      }
+
+      if(network && contractAddress){
+         const poolData = await fetcher<{data: PoolData[]}>(`/onchain/networks/${network}/tokens/${contractAddress}/pools`)
+
+         return poolData.data?.[0] ?? fallback
+      }
+
+      try{
+         const poolData = await fetcher<{data: PoolData[]}>('/onchain/search/pools', {query: id})
+         return poolData.data?.[0] ?? fallback
+      }catch {
+         return fallback
+      }
+   }
